7a4aa41708d12501152319eaea7e28e211aad9c5
na.alvarado <na.alvarado@uniandes.edu.co>
2019-01-17 11:59:35 -0500

    metodo para agregar una nota, se creo la table de notas

---
+++ SQL-Tablas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Notas(
+  Nombre TEXT NOT NULL PRIMARY KEY,
+  Porcentage REAL NOT NULL,
+  Puntos REAL NOT NULL,
+  Obtenida TEXT NOT NULL,
+  Clase TEXT NOT NULL
+);
--- src/Persist/NotaPersist.java
+++ src/Persist/NotaPersist.java
@@ -1,5 +1,41 @@
 package Persist;
 
+import java.sql.*;
+
 public class NotaPersist {
+	
+	private final static String url = "jdbc:sqlite:Notas.db";
+	
+	private Connection conn;
+	
+	public NotaPersist() {
+		try {
+			conn = DriverManager.getConnection(url);
+		}
+		catch(Exception e) {
+			System.out.println("No se pudo conectar a la base de datos :( " + e);
+		}
+		finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			}
+			catch(Exception e) {
+				System.out.println("Algo salio muy mal: " + e);
+			}
+		}
+	}
+	
+	public void agregarNota(String pNombre, double pPorcentage, double pPuntos, String pObtenida, String pClase) throws Exception {
+		String sql = "INSERT INTO Notas(Nombre,Porcentage,Puntos,Obtenida,Clase) VALUES(?,?,?,?,?)";
+		PreparedStatement ps = conn.prepareStatement(sql);
+		ps.setString(1, pNombre);
+		ps.setDouble(2, pPorcentage);
+		ps.setDouble(3, pPuntos);
+		ps.setString(4, pObtenida);
+		ps.setString(5, pClase);
+		ps.executeUpdate();
+	}
 
 }
